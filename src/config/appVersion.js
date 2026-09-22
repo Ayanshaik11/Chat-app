715c57063d11cb618f29ca5acd93fb63a44e9b2f
Ayan <ayanshaik11111@gmail.com>
2026-09-22 18:09:09 +0530

    Create appVersion.js for version control
    Add appVersion.js to manage application versioning.

---
+++ src/config/appVersion.js
@@ -0,0 +1,4 @@
+// Bump this number every time you build and share a new APK.
+// It must match the "latestVersion" you set in Firestore (config/appVersion)
+// when you want everyone to be notified of that build.
+export const CURRENT_VERSION = '1.0.0';
