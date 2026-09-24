331dc0c4de0e86716022db21058f40a5c71d5e69
Ayan <ayanshaik11111@gmail.com>
2026-09-24 17:46:07 +0530

    Update RootNavigator.js

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -82,7 +82,7 @@ export default function RootNavigator() {
   // wait for auth + my profile document
   if (initializing || (fbUser && !me)) {
     return (
-      <LinearGradient colors={['#050505', '#1a1600', '#F5B700']} style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <LinearGradient colors={['#050505', '#1a0505', '#E11D2A']} style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <ActivityIndicator color="#fff" size="large" />
       </LinearGradient>
     );
